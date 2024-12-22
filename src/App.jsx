1b9415b62ede4a6cccc1d50b705713d2bfb3e677
Hritik <khritik274@gmail.com>
2024-12-22 16:43:04 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import kgButton from "./components/KgButton";
+
+import KgButton from "./components/kgButton";
+// import Hello from "./Hello";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Hello Regal
+      </h1>
+      <KgButton />
+      
+      <KgButton />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/KgButton.jsx
@@ -0,0 +1,5 @@
+ function KgButton(){
+  return(<button>like this</button>)
+}
+
+export default KgButton;
